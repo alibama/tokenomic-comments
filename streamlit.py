a713304e8411d39e63fcde85d95b4741ba98729c
alibama <adp6j@virginia.edu>
2022-10-19 14:07:22 -0400

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -29,6 +29,6 @@ st.subheader('Exploratory Data Analysis with Streamlit')
 st.markdown('In this app we are using content pulled from the [tokenomic comments](https://tokenomics.aragond.tech) with a small Python script')
 
 @st.cache(suppress_st_warning=True)
-resp = requests.get('https://tokenomics.aragond.tech/topics.json?_format=json')
-df= pd.read_json(resp.text)
+
+df = pd.read_json('https://tokenomics.aragond.tech/topics.json?_format=json')
 print(df)
